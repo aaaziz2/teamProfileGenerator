e231b3a98e54f556d47d20e699862e59647f593f
aaaziz2 <aumio.aziz@gmail.com>
2022-08-13 16:09:45 -0500

    added all tests for Employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee')
 
 const name = 'Jorge'
-const id = '24'
+const id = 24
 const email = 'employee@work.com'
 
 describe('Employee', () => {
@@ -9,5 +9,23 @@ describe('Employee', () => {
         const employee = new Employee(name,id,email)
 
         expect(employee.getName()).toBe(name)
+    });
+
+    it('should have an ID', () => {
+        const employee = new Employee(name,id,email)
+
+        expect(employee.getId()).toBe(id)
+    });
+
+    it('should have an email', () => {
+        const employee = new Employee(name,id,email)
+
+        expect(employee.getEmail()).toBe(email)
+    });
+
+    it('should have role', () => {
+        const employee = new Employee(name,id,email)
+
+        expect(employee.getRole()).toBe('Employee')
     });
 });
